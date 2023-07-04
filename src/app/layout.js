517d451d7c8c177ee63d03070074297f1c29b766
Yury.Sakovich <yury.sakovich@mosaic.co>
2023-07-04 11:11:45 +0300

    update layout and main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
-import './globals.css';
-import './normalize.css';
 import { Inter } from 'next/font/google';
+
+import '@/styles/globals.css';
+import '@/styles/normalize.css';
+import PrimaryButton from '@/components/Buttons/primary';
+import LogoSvg from '@/assets/svg/logo';
+import styles from './layout.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,8 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <header>header</header>
+      <body className={inter.className} style={{ fontSize: '10px' }}>
+        <header className={styles.header}>
+          <div className={styles.labelWrapper}>
+            <LogoSvg />
+            <div className={styles.label}>Planes Live</div>
+          </div>
+          <PrimaryButton>Download app</PrimaryButton>
+        </header>
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import PlaneSvg from '@/assets/svg/plane';
+import Input from '@/components/Input';
+import TrafficBanner from '@/components/Banners/Landing/traffic';
+import BetterBanner from '@/components/Banners/Landing/better';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.grid}>
-        planes live web
+      <div className={styles.searchWrapper}>
+        Track
+        {' '}
+        <PlaneSvg />
+        <span className={styles.primaryText}>
+          flights
+        </span>
+        {' '}
+        <br />
+        worldwide
       </div>
+      <Input placeholder="Search by flight number, airline, or airport" />
+      <BetterBanner />
+      <TrafficBanner />
     </main>
   );
 }
