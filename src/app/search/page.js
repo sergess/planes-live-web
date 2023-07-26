1caa0b4413c8d89e9f6ccdc1f39a2338a369cc20
Yury.Sakovich <yury.sakovich@mosaic.co>
2023-07-26 16:44:43 +0300

    add desktop search

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 import SearchFlight from '@/components/SearchFlight';
+import SearchHeader from '@/components/Header/searchHeader';
+import Map from '@/components/Map';
 import styles from './page.module.css';
 
 export default async function Page() {
   return (
-    <div className={styles.container}>
-      <SearchFlight />
-    </div>
+    <>
+      <div className={styles.mobile}>
+        <SearchFlight />
+      </div>
+      <div className={styles.desktop}>
+        <div className={styles.desktopContainer}>
+          <SearchHeader />
+        </div>
+        <Map />
+      </div>
+    </>
   );
 }
