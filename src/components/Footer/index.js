14161fd0cfbe06e63989b265e9bf9737f5b8c73a
Yury.Sakovich <yury.sakovich@mosaic.co>
2023-07-31 17:08:16 +0300

    update footer spaces and linkgs

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import LogoSvg from '@/assets/svg/logo';
 import styles from './footer.module.css';
 
+const TERMS_REF = 'http://planeslive.com/terms';
+const POLICY_REF = 'http://planeslive.com/privacyPolicy';
+
 export default function Footer() {
   return (
     <footer className={styles.footer}>
@@ -12,13 +15,34 @@ export default function Footer() {
       </div>
       <div className={styles.blocks}>
         <p>Search for flights</p>
-        <p>Contact support</p>
+        <a
+          className={styles.support}
+          href="mailto:support@planeslive.app?subject=support"
+        >
+          Contact support
+        </a>
         <p>More apps</p>
       </div>
       <p className={styles.copyright}>
         Copyright @ 2022 Apalon Apps. All rights reserved.
       </p>
-      <p className={styles.privacy}>Privacy Policy | Terms</p>
+      <a
+        href={POLICY_REF}
+        target="_blank"
+        className={styles.privacy}
+        rel="noreferrer"
+      >
+        Privacy Policy
+      </a>
+      <span> | </span>
+      <a
+        href={TERMS_REF}
+        target="_blank"
+        className={styles.privacy}
+        rel="noreferrer"
+      >
+        Terms of use
+      </a>
     </footer>
   );
 }
