a991473503d9fb724a8dc3e57bc3010b56faa617
sergey_serjan <serguei.serjan@apalon.com>
2023-08-23 19:03:33 +0300

    fix(CustomMap): [PLAN-52] was fixed CustomMap

--- src/components/CustomMap/index.js
+++ src/components/CustomMap/index.js
@@ -5,13 +5,15 @@ import React, { useEffect, useRef } from 'react';
 import MapBox from '@/components/Map';
 import useScreenWidthSmallerThan from '@/hooks/useScreenWidthSmallerThan';
 
+const DESKTOP_WIDTH = 701;
+
 export default function CustomMap() {
-  const { isSmaller: isScreenMob, screenWidth } = useScreenWidthSmallerThan(600);
+  const { isSmaller: isScreenMob, screenWidth } = useScreenWidthSmallerThan(DESKTOP_WIDTH);
 
   const mapRef = useRef(null);
 
   useEffect(() => {
-    if (mapRef.current) {
+    if (!isScreenMob && mapRef.current) {
       mapRef.current.resize();
     }
   }, [isScreenMob, screenWidth]);
