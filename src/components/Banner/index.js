a0c04391f0741efe2e430ea501c19467d9a2caf8
Yury.Sakovich <yury.sakovich@mosaic.co>
2023-09-06 17:06:08 +0300

    update banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,34 +3,12 @@
 import React from 'react';
 
 import SmartBanner from 'react-smartbanner';
-import Image from 'next/image';
 
-import { PLATFORM } from '@/constants/index';
-import { getMobileOperatingSystem } from '@/utils/index';
-import styles from './Banner.module.scss';
-
-const IMG_SIZE = 16;
 export default function Banner() {
-  const icon = getMobileOperatingSystem() === PLATFORM.IOS
-    ? '/svg/ic_apple.svg'
-    : '/svg/ic_google_play.svg';
-
   return (
     <SmartBanner
       title="Planes Live"
-      button={(
-        <div className={styles.appleButtonContainer}>
-          <Image
-            src={icon}
-            width={IMG_SIZE}
-            height={IMG_SIZE}
-            alt="Platform icon"
-          />
-          <p>
-            Get
-          </p>
-        </div>
-)}
+      button="GET"
       storeText={{
         ios: 'Get more from the app',
         android: 'Get more from the app',
--- src/components/BannerMetaTags/index.js
+++ src/components/BannerMetaTags/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function BannerMetaTags({ path }) {
   return (
     <>
+      <meta name="smartbanner:disable-positioning" content="true" />
       <meta name="apple-itunes-app" content={`app-id=${process.env.IOS_STORE_ID}, app-argument=${path}`} />
       <meta name="google-play-app" content={`app-id=${process.env.ANDROID_STORE_ID}, app-argument=${path}`} />
       <link rel="apple-touch-icon" href="/svg/app_icon_with_bg.svg" />
