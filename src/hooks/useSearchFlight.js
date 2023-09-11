f8921033e74367917fecc3621fbed5808f6f242a
sergey_serjan <serguei.serjan@apalon.com>
2023-09-11 17:32:02 +0300

    feat(swr): [PLAN-69] use swr instead of request

--- src/hooks/useSearchFlight.js
+++ src/hooks/useSearchFlight.js
@@ -1,48 +1,47 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import {
   MIN_SYMBOL_COUNT, OPTION_TYPE, ROUTE_BY_TYPE,
 } from '@/constants/index';
-import { searchRequest } from '@/requests/index';
+import useMutationFetch from '@/hooks/useMutationFetch';
 
 export default () => {
   const router = useRouter();
-  const [options, setOptions] = React.useState([]);
-  const [text, setText] = React.useState('');
-  const [loading, setLoading] = React.useState(false);
+  const [text, setText] = useState('');
+  const [options, setOptions] = useState([]);
 
   const onChange = (e) => {
     setText(e.target.value);
   };
+
   const onSelect = (value, type) => {
     router.push(`/${ROUTE_BY_TYPE[type]}/${value}`);
   };
 
-  React.useEffect(() => {
-    const asyncFetch = async (t) => {
-      setLoading(true);
-      const response = await searchRequest(t);
+  const { trigger, isMutating: loading } = useMutationFetch('/api/search');
 
-      setLoading(false);
-
-      // [TODO] Think about better way how it could be done
-      if (response) {
-        setOptions([...response.airports
-          .map((a) => ({
-            label: a.name,
-            value: a.icao,
-            type: OPTION_TYPE.airport,
-          })), ...response.autosuggest
-          .map((a) => ({
-            label: a.code,
-            value: a.code,
-            type: OPTION_TYPE.flight,
-          }))]);
-      }
-    };
+  useEffect(() => {
     if (text.length > MIN_SYMBOL_COUNT) {
-      asyncFetch(text);
+      (async () => {
+        try {
+          const { data: resultOfSearch } = await trigger({ query: text });
+          setOptions([...resultOfSearch.airports
+            .map((a) => ({
+              label: a.name,
+              value: a.icao,
+              type: OPTION_TYPE.airport,
+            })), ...resultOfSearch.autosuggest
+            .map((a) => ({
+              label: a.code,
+              value: a.code,
+              type: OPTION_TYPE.flight,
+            }))]);
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.error('Something went wrong');
+        }
+      })();
     }
   }, [text]);
 
