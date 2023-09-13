a233e49f94b86c6a8235b75e77be1d3080b4bc6b
sergey_serjan <serguei.serjan@apalon.com>
2023-09-13 19:29:16 +0300

    feat(getNearestFlightIndex): [PLAN-64] getNearestFlightIndex

---
+++ src/utils/getNearestFlightIndex.js
@@ -0,0 +1,5 @@
+const nearestFlightDate = (arr, val) => arr.reduce((nearest, num) => Math.abs(num - val) >= Math.abs(nearest - val) && nearest < num? nearest : num);
+
+const getNearestFlightIndex = (arr, val) => arr.indexOf(nearestFlightDate(arr, val));
+
+export default getNearestFlightIndex;
