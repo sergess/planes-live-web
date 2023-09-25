e2dda90258a4cebd898c4a278bd0790ea58c1cd9
Yury.Sakovich <yury.sakovich@mosaic.co>
2023-09-25 09:51:43 +0300

    add go to main page handling for not found error

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,10 +1,20 @@
+'use client';
+
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 import ErrorPage from '@/components/ErrorPage';
 import { ERROR_CODE } from '@/constants/index';
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
-    <ErrorPage code={ERROR_CODE.NOT_FOUND} />
+    <ErrorPage
+      code={ERROR_CODE.NOT_FOUND}
+      onClick={
+      () => router.push('/')
+    }
+    />
   );
 }
