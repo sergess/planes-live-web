dc8eb03486cf309fae133e649f672ed7d087da28
Yury.Sakovich <yury.sakovich@mosaic.co>
2023-09-28 14:57:13 +0300

    remove process.cwd temporary

--- src/utils/writeFile.js
+++ src/utils/writeFile.js
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const JSON_PATH = `${process.cwd()}/cache/commonData.json`;
+const JSON_PATH = 'cache/commonData.json';
 
 export const writeJSON = (json) => {
   fs.writeFileSync(JSON_PATH, JSON.stringify(json));
