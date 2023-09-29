4f0d98c2af23a62f9b50491009c14e6cacf094be
sergey_serjan <serguei.serjan@apalon.com>
2023-09-29 12:42:45 +0300

    fix(Bag claim icon): [PLAN-122] Bag claim icon(suitcase.svg)

--- src/components/GateCard/index.js
+++ src/components/GateCard/index.js
@@ -4,16 +4,17 @@ import Image from 'next/image';
 import styles from './gate.module.css';
 
 const DEPARTURE_ICON = '/svg/gate_departure_alt.svg';
-const ARRIVE_ICON = '/svg/gate_arrival_alt.svg';
+const LUGGAGE_ICON = '/svg/suitcase.svg';
 
 export default function Gate({ isDeparture = false, gate }) {
   return (
     <div className={styles.container}>
       <Image
-        src={isDeparture ? DEPARTURE_ICON : ARRIVE_ICON}
+        src={isDeparture ? DEPARTURE_ICON : LUGGAGE_ICON}
         alt={isDeparture ? 'departure gate icon' : 'arrival gate icon'}
         width={16}
         height={16}
+        style={{marginLeft: !isDeparture && '2px'}}
       />
       {gate}
     </div>
