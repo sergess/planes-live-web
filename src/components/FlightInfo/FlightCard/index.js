43ca9b761fb21b23be8fca4d7c8c521f72bea627
Yury.Sakovich <yury.sakovich@mosaic.co>
2023-10-09 15:52:09 +0300

    update flight info card

--- src/components/FlightInfo/FlightCard/index.js
+++ src/components/FlightInfo/FlightCard/index.js
@@ -12,7 +12,6 @@ import { EMPTY_FIELD } from '@/constants/index';
 import flightContext from '@/contexts/flight/FlightContext';
 import { getDateDifferenceHM } from '@/utils/date';
 
-import usePathLine from '@/hooks/usePathLine';
 import styles from './flightCard.module.scss';
 
 const AIRLINE_PLACEHOLDER = '/svg/airline_placeholder.svg';
@@ -38,11 +37,9 @@ const getCodes = (flight, flightNumber, flightId) => {
     id,
   };
 };
-const TOTAL_BLOCK_HEIGHT = 47;
+
 export default function FlightCard({ logoUrl, flightId, flightNumber }) {
   const { flightData } = useContext(flightContext);
-  // to extract path line size css
-  const { height, blockRef } = usePathLine();
   if (!flightData?.flight || !flightData?.destinationAirport || !flightData?.departureAirport) {
     return null;
   }
@@ -114,68 +111,69 @@ export default function FlightCard({ logoUrl, flightId, flightNumber }) {
         />
       </div>
       <div className={styles.boxBody}>
-        <div
-          className={styles.line}
-        >
-          <div
-            className={styles.lineItem}
-            style={{
-              height: height + TOTAL_BLOCK_HEIGHT,
-            }}
-          />
-        </div>
         <div className={styles.container}>
-          <div className={styles.block} ref={blockRef}>
-            <div className={styles.blockContainer}>
-              <p className={styles.title}>{city}</p>
-              <Link
-                href={`/airport/${departureIcao}`}
-                className={styles.description}
-                title={name}
-                prefetch={false}
-              >
-                {originIata}
-                {' '}
-                —
-                {' '}
-                {name}
-              </Link>
-              <p className={styles.info}>
-                Terminal
-                {` ${departureTerminal || EMPTY_FIELD}`}
-                {' '}
-                • Check-in
-                {' '}
-                {departureCheckInDesk || EMPTY_FIELD}
-                {' '}
-                • Gate
-                {' '}
-                {departureGate || EMPTY_FIELD}
-              </p>
+          <div className={styles.lineContainer}>
+            <div
+              className={styles.line}
+            >
+              <div
+                className={styles.lineItem}
+              />
             </div>
-            <Time time={departureTime} actual={actualDepartureTime} tz={departureTz} />
-          </div>
-          <div className={styles.middle}>
-            <Image
-              src="/svg/hor_line.svg"
-              width={105}
-              height={2}
-              alt=""
-            />
-            <p className={styles.timeDiff}>
-              Total
-              {` ${total}`}
-            </p>
-            <Image
-              src="/svg/hor_line.svg"
-              width={105}
-              height={2}
-              alt=""
-            />
-          </div>
-          <div className={styles.block}>
-            <div className={styles.blockContainer}>
+            <div className={styles.pathWrapper}>
+              <div className={styles.block}>
+                <div className={styles.blockContainer}>
+                  <p className={styles.title}>{city}</p>
+                  <Link
+                    href={`/airport/${departureIcao}`}
+                    className={styles.description}
+                    title={name}
+                    prefetch={false}
+                  >
+                    {originIata}
+                    {' '}
+                    —
+                    {' '}
+                    {name}
+                  </Link>
+                  <p className={styles.info}>
+                    Terminal
+                    {` ${departureTerminal || EMPTY_FIELD}`}
+                    {' '}
+                    • Check-in
+                    {' '}
+                    {departureCheckInDesk || EMPTY_FIELD}
+                    {' '}
+                    • Gate
+                    {' '}
+                    {departureGate || EMPTY_FIELD}
+                  </p>
+                </div>
+                <Time time={departureTime} actual={actualDepartureTime} tz={departureTz} />
+              </div>
+              <div className={styles.middle}>
+                <Image
+                  src="/svg/hor_line.svg"
+                  width={105}
+                  height={2}
+                  alt=""
+                />
+                <p className={styles.timeDiff}>
+                  Total
+                  {` ${total}`}
+                </p>
+                <Image
+                  src="/svg/hor_line.svg"
+                  width={105}
+                  height={2}
+                  alt=""
+                />
+              </div>
               <p className={styles.title}>{destinationCity}</p>
+            </div>
+          </div>
+          <div className={`${styles.bottomBlock} bottomBlock`}>
+            <div className={styles.blockContainer}>
               <Link
                 href={`/airport/${destinationIcao}`}
                 className={styles.description}
