da78fc6442ea51ed33c0a79303a347cfd4079a22
sergey_serjan <serguei.serjan@apalon.com>
2023-10-09 14:09:30 +0300

    fix(path with): [PLAN-141] plane doesn't match width

--- src/components/Swipe/FlightPreview/FlightProgress/index.js
+++ src/components/Swipe/FlightPreview/FlightProgress/index.js
@@ -4,7 +4,14 @@ import Image from 'next/image';
 import styles from './FlightProgress.module.scss';
 
 const IMG_SIZE = 16;
+const SPACE_RIGHT = 1;
+const SPACE_LEFT = 2.5;
+
 export default function FlightProgress({ value = 0 }) {
+  const style = value < IMG_SIZE
+    ? { left: `calc(${value}% - ${SPACE_LEFT}px)`, top: '0.5px' }
+    : { left: `calc(${value}% - ${IMG_SIZE - SPACE_RIGHT}px)`, top: '0.5px' };
+
   return (
     <div className={styles.container}>
       <div className={styles.passed} style={{ width: `${value}%` }}>.</div>
@@ -15,7 +22,7 @@ export default function FlightProgress({ value = 0 }) {
         height={IMG_SIZE}
         alt="Plane symbol"
         className={styles.plane}
-        style={{ left: `calc(${value}% - (${IMG_SIZE}px / 2))` }}
+        style={style}
       />
     </div>
   );
