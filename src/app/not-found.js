8a815a1c0b02e7bd24eb5ea787a24e4af14d8b7c
Yury.Sakovich <yury.sakovich@mosaic.co>
2023-10-10 15:21:33 +0300

    update reset props

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,11 +6,12 @@ import { useRouter } from 'next/navigation';
 import ErrorPage from '@/components/ErrorPage';
 import { ERROR_CODE } from '@/constants/index';
 
-export default function NotFound() {
+export default function NotFound(props) {
   const router = useRouter();
 
   return (
     <ErrorPage
+      {...props}
       code={ERROR_CODE.NOT_FOUND}
       onClick={
       () => router.push('/')
