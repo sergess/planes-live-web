d50b1937a3b7bd0e1edeb4fc30c5b1dd3f61faee
yury.sakovich <yury.sakovich@mosaic.co>
2023-12-04 14:20:19 +0300

    add min len validation + new texts

--- src/components/CompletedScreen/index.js
+++ src/components/CompletedScreen/index.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import Image from 'next/image';
-
+import { Inter } from 'next/font/google';
 import styles from './CompletedScreen.module.scss';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function CompletedScreen() {
   return (
-    <div className={styles.successContainer}>
+    <div className={`${styles.successContainer} ${inter.className}`}>
       <div className={styles.wrapper}>
         <Image
           src="/svg/check_filled.svg"
@@ -17,9 +19,14 @@ export default function CompletedScreen() {
           alt="Left arrow"
         />
         <h1>
-          Your password has been
-          successfully changed
+          Password changed
         </h1>
+        <p className={styles.text}>
+          You can now sign in using
+          {' '}
+          <br />
+          your new password
+        </p>
       </div>
     </div>
   );
