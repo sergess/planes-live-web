11f04e07f5781c93dc60c2aa7a2652341801398d
sergey_serjan <serguei.serjan@apalon.com>
2023-12-12 19:36:36 +0300

    feat(slots): [PLAN-176] were filled slots on home and search route

--- src/constants/adOptions.js
+++ src/constants/adOptions.js
@@ -37,6 +37,64 @@ export const AD_OPTIONS = {
       0: [],
     },
   },
+  'div-gpt-ad-1702387091622-0': {
+    selector: 'spot-home-center3',
+    slot: '/22654782375/ca-pub-2575005206305234-tag/PlanesLive_728x90-1',
+    sizes: [[728, 90], [320, 50]],
+    mapping: {
+      740: [728, 90],
+      350: [320, 50],
+      0: [],
+    },
+  },
+  'div-gpt-ad-1702387224332-0': {
+    selector: 'spot-home-center4',
+    slot: '/22654782375/ca-pub-2575005206305234-tag/PlanesLive_728x90_4_Home_Page',
+    sizes: [[728, 90], [320, 50]],
+    mapping: {
+      740: [728, 90],
+      350: [320, 50],
+      0: [],
+    },
+  },
+  'div-gpt-ad-1702387987050-0': {
+    selector: 'spot-home-center5',
+    slot: '/22654782375/ca-pub-2575005206305234-tag/PlanesLive_728x90-5_Home_Page',
+    sizes: [[728, 90], [320, 50]],
+    mapping: {
+      740: [728, 90],
+      350: [320, 50],
+      0: [],
+    },
+  },
+  'div-gpt-ad-1702388062757-0': {
+    selector: 'spot-home-center6',
+    slot: '/22654782375/ca-pub-2575005206305234-tag/PlanesLive_728x90_6_Home_Page',
+    sizes: [[728, 90], [320, 50]],
+    mapping: {
+      740: [728, 90],
+      350: [320, 50],
+      0: [],
+    },
+  },
+  'div-gpt-ad-1702388746229-0': {
+    selector: 'spot-map-right',
+    slot: '/22654782375/ca-pub-2575005206305234-tag/PlanesLive_160x600_Map',
+    sizes: [[160, 600]],
+    mapping: {
+      740: [160, 600],
+      0: [],
+    },
+  },
+  'div-gpt-ad-1702398586595-0': {
+    selector: 'spot-map-bot',
+    slot: '/22654782375/ca-pub-2575005206305234-tag/PlanesLive_336x280_1_Map_Search',
+    sizes: [[320, 50], [336, 280]],
+    mapping: {
+      702: [336, 280],
+      0: [320, 50],
+    },
+  },
 };
 
 export default AD_OPTIONS;
